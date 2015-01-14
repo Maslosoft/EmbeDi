022667a35ada71b89a0c62095a134e07217b0bcf
Peter Maselkowski <pmaselkowski@gmail.com>
2015-01-14 19:37:38 +0100

    +~ export and apply

---
+++ make-junctions.bat
@@ -0,0 +1,2 @@
+mklink /j unit vendor\maslosoft\embeditest\tests\unit
+mklink /j models vendor\maslosoft\embeditest\models
--- src/EmbeDi.php
+++ src/EmbeDi.php
@@ -85,11 +85,46 @@ class EmbeDi
 	public function apply($object, $configuration)
 	{
 		// TODO Apply configuration to object from array
+		foreach($configuration as $name => $value)
+		{
+			if(is_array($value) && array_key_exists($this->classField, $value))
+			{
+				$className = $value[$this->classField];
+				unset($value[$this->classField]);
+				$subObject = new $className;
+				$object->$name = $this->apply($subObject, $value);
+			}
+			else
+			{
+				$object->$name = $value;
+			}
+		}
+		return $object;
 	}
 
-	public function export()
+	/**
+	 * Export configuration
+	 * @param object $object
+	 * @param string[] $fields
+	 * @return mixed[][]
+	 */
+	public function export($object, $fields = [])
 	{
 		// TODO Export current configuration to array
+		foreach ($this->_getFields($object, $fields) as $name)
+		{
+			// If object, recurse
+			if (is_object($object->$name))
+			{
+				$data[$name] = $this->export($object->$name);
+				$data[$this->classField] = get_class($object->$name);
+			}
+			else
+			{
+				$data[$name] = $object->$name;
+			}
+		}
+		return $data;
 	}
 
 	public function store($object, $fields = [])
