50c46a48a5d819d5a6962d35a7c439e02a6a4536
Peter Maselkowski <pmaselkowski@gmail.com>
2015-03-10 11:44:40 +0100

    * licenses

--- run-tests.bat
+++ run-tests.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET BIN_TARGET=%~dp0/vendor/phpunit/phpunit/phpunit
-php "%BIN_TARGET%" --colors --bootstrap vendor/maslosoft/embeditest/bootstrap.php vendor/maslosoft/embeditest/tests
+php "%BIN_TARGET%" --colors --bootstrap vendor\maslosoft\embeditest\bootstrap.php vendor\maslosoft\embeditest\tests
