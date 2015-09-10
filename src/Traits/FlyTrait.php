dda3b7ba533ccaa0fe368a89438c4d671d8af29f
Peter Maselkowski <pmaselkowski@gmail.com>
2015-09-10 10:51:23 +0200

    + FlyTrait possibility to use DefaultInstanceId

--- src/Traits/FlyTrait.php
+++ src/Traits/FlyTrait.php
@@ -2,16 +2,28 @@
 
 namespace Maslosoft\EmbeDi\Traits;
 
+use Exception;
+
 trait FlyTrait
 {
+
 	private static $instances = [];
-	
-	public static function fly($instanceId)
+
+	public static function fly($instanceId = null)
 	{
-		if(!isset(self::$instances[static::class][$instanceId]))
+		if (null === $instanceId)
+		{
+			if (!defined(sprintf('%s::%s', static::class, 'DefaultInstanceId')))
+			{
+				throw new Exception(sprintf('Class `%s` must define constant `%s` (%1$s::%2$s)', static::class, 'DefaultInstanceId'));
+			}
+			$instanceId = static::DefaultInstanceId;
+		}
+		if (!isset(self::$instances[static::class][$instanceId]))
 		{
 			self::$instances[static::class][$instanceId] = new static($instanceId);
 		}
 		return self::$instances[static::class][$instanceId];
 	}
+
 }
