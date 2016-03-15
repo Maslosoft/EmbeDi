eaa6c54035b4d356620c43d709dcdee08655aa25
Peter Maselkowski <pmaselkowski@gmail.com>
2016-03-15 20:29:57 +0100

    * Class headers

--- src/Traits/FlyTrait.php
+++ src/Traits/FlyTrait.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This software package is licensed under `AGPL, Commercial` license[s].
+ *
+ * @package maslosoft/embedi
+ * @license AGPL, Commercial
+ *
+ * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
+ *
+ */
+
 namespace Maslosoft\EmbeDi\Traits;
 
 use Exception;
