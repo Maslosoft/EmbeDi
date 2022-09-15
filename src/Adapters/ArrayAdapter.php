590bb5549ca64384f011d9e04386865ff277da61
Peter Maselkowski <office@maslosoft.com>
2022-09-15 15:27:58 +0200

    * Class headers

--- src/Adapters/ArrayAdapter.php
+++ src/Adapters/ArrayAdapter.php
@@ -1,13 +1,15 @@
 <?php
 
 /**
+ * Embedded Dependency Injection container
+ *
  * This software package is licensed under `AGPL, Commercial` license[s].
  *
  * @package maslosoft/embedi
  * @license AGPL, Commercial
  *
  * @copyright Copyright (c) Peter Maselkowski <pmaselkowski@gmail.com>
- *
+ * @link https://maslosoft.com/embedi/
  */
 
 namespace Maslosoft\EmbeDi\Adapters;
